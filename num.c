6434d3b4e500c633657d04ab1915250985553efe
adithak <adithak59@gmail.com>
2018-09-29 16:07:19 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
